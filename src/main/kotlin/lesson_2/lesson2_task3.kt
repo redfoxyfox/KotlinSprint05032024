5ce35fda40a87dc57b062d0f770e1fb24316b587
Sergej Nikulin <etci3200@gmail.com>
2024-03-07 19:29:27 +0100

    KS-2-3 I solved it using arithmetic

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -14,21 +14,30 @@ const val NUMBER_OF_MINUTES_IN_AN_HOUR = 60
 
 fun main() {
     val checkOutHour = 9
-    val checkOutMinute = 39
+    val checkOutMinutes = 39
     val travelTime = 457
 
-    // Запись в формате времени
-    val time = java.time.LocalTime.of(checkOutHour, checkOutMinute)
+    // Перевод часов отправления в минуты
+    val checkOutHourToMinutes = checkOutHour * NUMBER_OF_MINUTES_IN_AN_HOUR
 
-    // Перевод минут в часы и минуты
-    val travelHour = travelTime / NUMBER_OF_MINUTES_IN_AN_HOUR
-    val travelMinute = travelTime % NUMBER_OF_MINUTES_IN_AN_HOUR
+    val checkOutTotal = checkOutHourToMinutes + checkOutMinutes
+    val totalMinutes = checkOutTotal + travelTime
 
-    // Прибавление ко времени старта времени в пути
-    var arrivalTime = time.plusHours(travelHour.toLong())
-    arrivalTime = arrivalTime.plusMinutes(travelMinute.toLong())
+    val arrivalTimeHour = totalMinutes / NUMBER_OF_MINUTES_IN_AN_HOUR
+    val arrivalTimeMinutes = totalMinutes % NUMBER_OF_MINUTES_IN_AN_HOUR
 
-    println(arrivalTime)
+    println("$arrivalTimeHour:$arrivalTimeMinutes")
+
+    /* Другое решение
+       // Запись в формате времени
+        val time = java.time.LocalTime.of(checkOutHour, checkOutMinutes)
+
+        // Перевод минут в часы и минуты
+        val travelHour = travelTime / NUMBER_OF_MINUTES_IN_AN_HOUR
+        val travelMinute = travelTime % NUMBER_OF_MINUTES_IN_AN_HOUR
+
+        // Прибавление ко времени старта времени в пути
+        var arrivalTime = time.plusHours(travelHour.toLong())
+        arrivalTime = arrivalTime.plusMinutes(travelMinute.toLong())
+        println(arrivalTime)*/
 }
-
-
