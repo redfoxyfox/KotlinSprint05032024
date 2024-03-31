21beef12cc9482c4ac6b470bfbff5b9e101948c1
Sergej Nikulin <etci3200@gmail.com>
2024-03-31 21:35:00 +0200

    KS-11-2 methods in class

--- src/main/kotlin/lesson_11/lesson11_task2.kt
+++ src/main/kotlin/lesson_11/lesson11_task2.kt
@@ -6,22 +6,53 @@ package lesson_11
 
 - вывод информации о пользователе в консоль;
 - считывание из консоли текста и запись в поле bio;
-- изменение пароля — сначала запросить текущий пароль, и, если он введен верно, запросить новый. Записать его в
-поле и сообщить, что пароль изменен;
+- изменение пароля — сначала запросить текущий пароль, и, если он введен верно, запросить новый. Записать его в поле
+и сообщить, что пароль изменен;
 
 Далее создай объект, заполни информацию “о себе” и замени пароль. В конце выведи обновленную информацию о пользователе.*/
 
+class User2(
+    val id: Int,
+    val login: String,
+    var password: String,
+    val email: String,
+    var bio: String = "",
+) {
+    fun outputInformation() {
+        println(id)
+        println(login)
+        println(password)
+        println(email)
+        println(bio)
+    }
+
+    fun addBio() {
+        println("Напишите биографию")
+        bio = readln()
+    }
+
+    fun changePassword() {
+        println("Введите старый пароль")
+        val oldPassword = readln()
+        if (oldPassword == password) {
+            println("Введите новый пароль")
+            password = readln()
+            println("Пароль изменён")
+        } else println("Неправильно. Вы кто такие?")
+    }
+}
+
 fun main() {
 
-    val user = User2(
+    val user1 = User2(
         id = 1,
-        login = "Kruz",
-        password = "qwerty789",
-        email = "kruzcastillo@gmail.com",
+        login = "Cici",
+        password = "qwerty123",
+        email = "cicicapwell@gmail.com",
     )
 
-    user.biography()
-    user.changePassword()
-    user.displayingUserInformation()
+    user1.addBio()
+    user1.changePassword()
+    user1.outputInformation()
 
 }
