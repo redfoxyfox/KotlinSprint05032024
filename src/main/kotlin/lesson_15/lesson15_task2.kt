7ded417d3a10be6a76ab3a036e3f906186874099
Sergej Nikulin <etci3200@gmail.com>
2024-04-15 22:38:08 +0200

    KS-15-2 add class WeatherServer

--- src/main/kotlin/lesson_15/lesson15_task2.kt
+++ src/main/kotlin/lesson_15/lesson15_task2.kt
@@ -3,8 +3,8 @@ package lesson_15
 /*
 Задача 2 к Уроку 15
 
-Необходимо разработать простую систему мониторинга погодных условий, которая будет собирать данные о температуре и количестве осадков.
-
+Необходимо разработать простую систему мониторинга погодных условий, которая будет собирать данные о температуре и
+количестве осадков.
 
 Создай абстрактный класс WeatherStationStats. Он будет служить родителем для классов-объектов погодных данных:
 Temperature и PrecipitationAmount.
@@ -16,12 +16,6 @@ Temperature и PrecipitationAmount.
 
 abstract class WeatherStationStats {
     abstract val weatherData: Any
-
-    fun sendingData() {
-        if (weatherData is Int) println("Температура сейчас $weatherData градусов")
-        else if (weatherData is String) println("Сейчас $weatherData осадков")
-    }
-
 }
 
 class Temperature(
@@ -32,13 +26,27 @@ class PrecipitationAmount(
     override val weatherData: String,
 ) : WeatherStationStats()
 
+class WeatherServer() {
+
+    fun sendingData(weatherData: WeatherStationStats) {
+        when (weatherData.weatherData) {
+            is Int -> println("Температура сейчас ${weatherData.weatherData} градусов")
+            is String -> println("Сейчас ${weatherData.weatherData} осадков")
+        }
+    }
+
+}
+
 fun main() {
 
     val temperature = Temperature(15)
     val precipitationAmount = PrecipitationAmount("мало")
 
-    temperature.sendingData()
-    precipitationAmount.sendingData()
+    val weatherServer = WeatherServer()
+
+    weatherServer.sendingData(temperature)
+    weatherServer.sendingData(precipitationAmount)
+
 }
 
 
